8225cc304c11345826a70696b28a8ef8c1f23911
Schousboe <jakobschousboe@hotmail.com>
2025-08-14 21:08:34 +0000

    Fetch call made

---
+++ script.js
@@ -0,0 +1,17 @@
+const backendURL = "https://army-division-backend.onrender.com";
+
+document.querySelector(".login-container").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value;
+
+  const res = await fetch(`${backendURL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+  console.log(data);
+});
